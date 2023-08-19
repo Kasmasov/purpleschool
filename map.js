3ea7b3b50b6892d1c7336267dff3c7185e59968f
kasmasov <mdg@bk.ru>
2023-08-20 00:51:34 +0300

    ready for verification

--- map.js
+++ map.js
@@ -8,4 +8,18 @@ let wetherMap = new Map ([
 
 // поменять местами ключи и значения
 const changeValuesToKeys = new Map([...wetherMap].map((item) => item.reverse()))
-console.log(changeValuesToKeys);
+// console.log(changeValuesToKeys);
+
+// Сделать с помощью Set уникализацию массива обектов
+
+const users = [
+    { id: 1, name: 'Вася'},
+    { id: 2, name: 'Петя'},
+    { id: 1, name: 'Вася'},
+];
+
+const uniqueUsersById = [...new Set(users.map((user) => user.id))]
+  .map((userId) => users.find((user) => userId === user.id));
+
+console.log(uniqueUsersById);
+
