d0a9377a56272e75b3d6a7f561ea61b5f1a2b1eb
kasmasov <mdg@bk.ru>
2023-08-20 22:43:52 +0300

    rewrite function isBirthday

--- time.js
+++ time.js
@@ -3,14 +3,14 @@ const user = {
     birthday: '08/20/2023',
 }
 
-function isBirthdayToday (date) {
+function isBirthdayToday (user) {
 
-	const getSummOfDate = (date) => new Date(date).getDate() +  new Date(date).getMonth() + new Date(date).getFullYear();
-	const today = getSummOfDate(new Date());
-	const dateToBeCheked = getSummOfDate(new Date (date));	
-	if (today === dateToBeCheked) return true;
+	const today = new Date();
+	const birthdayDate = new Date(user.birthday);
+	if (today.getDate() !== birthdayDate.getDate()) return false;
+	if (today.getMonth() !== birthdayDate.getMonth()) return false;
 
-	return false;
+	return true;
 }
 
-console.log(isBirthdayToday(user.birthday));
+console.log(isBirthdayToday(user));
