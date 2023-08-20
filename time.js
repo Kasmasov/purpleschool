7e3798bb5c114ab7990fe9e2a803414dde11961e
kasmasov <mdg@bk.ru>
2023-08-21 01:07:18 +0300

    write check up age function

--- time.js
+++ time.js
@@ -13,4 +13,25 @@ function isBirthdayToday (user) {
 	return true;
 }
 
-console.log(isBirthdayToday(user));
+// console.log(isBirthdayToday(user));
+
+function isAdult (date) {
+	const age = 14;
+  const today = new Date();
+	const birthdayDate = new Date(date);
+
+	if (today.getFullYear() - birthdayDate.getFullYear() > age) {
+		return true;
+	} else if(today.getFullYear() - birthdayDate.getFullYear() === age) {
+    if (today.getMonth() > birthdayDate.getMonth()) {
+			return true;
+		} else if (today.getMonth() === birthdayDate.getMonth()) {
+			if (today.getDate() >= birthdayDate.getDate()) {
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+console.log(isAdult('2009-08-22'))
