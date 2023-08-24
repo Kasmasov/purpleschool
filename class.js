7ebcf14c0eb772f1edd757cde70def82c3e8b4e9
kasmasov <kosmos4v@gmail.com>
2023-08-24 19:37:08 +0300

    write class Car homework

--- class.js
+++ class.js
@@ -65,3 +65,37 @@ user1.getLoginAndPassword('abc')
 user1.changePassword('abc', 'xwz');
 user1.login = 'dsfdsfs';
 console.log(user1)
+
+
+class Car {
+	#_brend;
+	#_model;
+	#_mileage;
+
+	constructor(brend, model, mileage) {
+		this.#brend = brend;
+		this.#model = model;
+		this.#mileage = mileage;
+	}
+
+	set #brend (brend) {
+		this.#_brend = brend;
+	}
+	
+	set #model (model) {
+		this.#_model = model;
+	}
+
+	set #mileage (mileage) {
+		this.#_mileage = mileage;
+	}
+
+	info() {
+		console.log(`brend: ${this.#_brend}, model: ${this.#_model}, mileage: ${this.#_mileage}`)
+	}
+
+}
+
+const bmw = new Car('BMW', 'X-5', 100000)
+console.log('bmw', bmw)
+bmw.info();
