19cce104e26093a452496427bbf343898b403c3c
kasmasov <kosmos4v@gmail.com>
2023-08-24 15:12:56 +0300

    create oop file with two characters: ork and elf

--- oop.js
+++ oop.js
@@ -47,4 +47,38 @@ cart.addProduct(product);
 cart.increaseAmout(1);
 cart.decreaseAmout(1);
 
-console.log(cart)
+const Character = function (name, race, language) {
+  this.name = name;
+	this.race = race;
+	this.language = language;
+}
+
+Character.prototype.speak = function () {
+	console.log(`${this.name}, ${this.language}`);
+}
+
+Character.prototype.weapons = function (weapons) {
+	this.weapons = weapons;
+}
+
+Character.prototype.stab = function () {
+	console.log('I stab you!')
+}
+
+const ork = new Character('John', 'green', 'thaba-thaba')
+ork.weapons = 'Axe';
+ork.stab();
+
+ork.speak();
+console.log(ork);
+ork.stab();
+
+Character.prototype.spell = function (spell, spellType) {
+	this.spell = spell;
+	this.spellType = spellType;
+	console.log(`${spell} - I conjure you with a ${spellType}`);
+};
+
+const elf = new Character('Robin', 'red', 'uba-tyba');
+console.log(elf);
+elf.spell('hyka-muka-kreks-peks', 'spell for power');
