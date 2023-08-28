68bec7eabf5bd8fb7b51e5647e26199f7228cba2
kasmasov <kosmos4v@gmail.com>
2023-08-28 23:04:06 +0300

    write with fuctions

--- promise.js
+++ promise.js
@@ -4,15 +4,35 @@ body.prepend(select);
 
 
 fetch('https://dummyjson.com/products/categories')
-.then(response =>  response.json())
+.then(response => response.json())
 .then(categories => {
 	categories
-	  .map((categorie, index) => {
-			console.log(categorie);
+	  .map((category, index) => {
 		  const option = document.createElement('option');
 		  option.value = index;
-		  option.text = categorie;
+		  option.text = category;
 		  select.append(option);
 	  });
 })
 .catch(error => console.log(error.message));
+
+// решение через функции и innerHTML
+
+const filter = document.createElement('div');
+filter.style.marginBottom = '20px';
+body.prepend(filter);
+
+function createSelect(array) {
+  filter.innerHTML = `<select>
+	  ${array.map((item) =>`<option value=${item}>${item}</option>` )}
+	</select>`
+}
+
+function getCategories(url) {
+  fetch(url)
+	.then((response) => response.json())
+	.then((data) => {createSelect(data)})
+	.catch(error => console.error(`Ошибка: ${error.message}`))
+}
+
+getCategories('https://dummyjson.com/products/categories');
