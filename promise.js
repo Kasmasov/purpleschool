121db9c5d6278452120ddf0a359140ff9f72ab83
kasmasov <kosmos4v@gmail.com>
2023-08-29 00:06:53 +0300

    write error function

--- promise.js
+++ promise.js
@@ -22,6 +22,15 @@ const filter = document.createElement('div');
 filter.style.marginBottom = '20px';
 body.prepend(filter);
 
+const errors = {
+  404: 'Not Found',
+}
+
+function isError(status) {
+  if (status === 200) return;
+	throw new Error(`${errors[status]}`)
+}
+
 function createSelect(array) {
   filter.innerHTML = `<select>
 	  ${array.map((item) =>`<option value=${item}>${item}</option>` )}
@@ -29,10 +38,13 @@ function createSelect(array) {
 }
 
 function getCategories(url) {
-  fetch(url)
-	.then((response) => response.json())
-	.then((data) => {createSelect(data)})
-	.catch(error => console.error(`Ошибка: ${error.message}`))
+  return fetch(url)
+	  .then((response) => {
+		  isError(response.status);
+		  return response.json();
+	  })
+	  .then((data) => {createSelect(data)})
+	  .catch(error => filter.innerHTML = `Ошибка: ${error.message}`)
 }
 
 getCategories('https://dummyjson.com/products/categories');
