bb039dae4f1a2b1ba9378c05a8f3ff407103166a
kasmasov <kosmos4v@gmail.com>
2023-08-30 13:28:25 +0300

    10-pokemon-promise

---
+++ promise_homework.js
@@ -0,0 +1,22 @@
+function getRequest(url, errorMessage) {
+  return fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`${errorMessage} ${response.status}`)
+      }
+      return response.json();
+    })
+};
+
+const errorText = 'Проблеммы с запросом, Status = '
+
+getRequest('https://pokeapi.co/api/v2/pokemon/ditto', errorText)
+  .then(({ abilities }) => {
+		console.log(abilities)
+		return getRequest(abilities[0].ability.url, errorText)
+	})
+	.then(response => {
+		const pokemonEffect = response.effect_entries.find((item) => item.language.name === 'en' ).effect;
+		console.log(pokemonEffect);
+	})
+	.catch(error => console.log(error));
