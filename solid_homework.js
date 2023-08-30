d5d51766fe04746b22e3ec5f7f9bca40a7bd8510
kasmasov <kosmos4v@gmail.com>
2023-08-30 14:56:38 +0300

    write second way for classes

--- solid_homework.js
+++ solid_homework.js
@@ -32,3 +32,16 @@ const item = new ItemBilling(30)
 console.log('bill', bill.calculateTotal(fix));
 console.log('hour', bill.calculateTotal(hour));
 console.log('item', bill.calculateTotal(item));
+
+
+class Ratio {
+	constructor(value) {
+    this.value = value;
+	}
+}
+
+const fixBilling = new Ratio(1);
+const hourRatio = new Ratio(50);
+const itemRatio = new Ratio(100);
+console.log('fixBilling', bill.calculateTotal(fixBilling));
+console.log('hourBilling', bill.calculateTotal(itemRatio));
