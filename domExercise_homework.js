c1e8f8137f6ca2687bc42fd8ba3293f637011fa0
kasmasov <kosmos4v@gmail.com>
2023-09-02 00:28:38 +0300

    13-button ready to check

---
+++ domExercise_homework.js
@@ -0,0 +1,46 @@
+const body = document.body;
+
+const wrapper = document.createElement('div');
+wrapper.setAttribute('style', 'display: flex; flex-direction: column; align-items: center; justify-content: center;');
+
+const buttonGroup = document.createElement('div');
+buttonGroup.style.display = 'flex';
+buttonGroup.style.gap = '12px';
+buttonGroup.style.marginBottom = '12px';
+
+
+const count = document.createElement('div');
+let counter = 0;
+count.textContent = counter;
+count.style.fontSize = '32px';
+
+body.prepend(wrapper);
+wrapper.prepend(buttonGroup);
+wrapper.append(count);
+
+for (let i = 0; i < 5; i++) {
+  const button = document.createElement('button');
+  button.textContent = 'Нажми меня!';
+  button.style.padding = '8px 8px';
+  button.style.width = '100px';
+  button.style.background = 'white';
+  button.style.border = '1px solid grey';
+  button.style.borderRadius = '4px';
+  button.style.cursor = 'pointer';
+  button.style.userSelect = 'none';
+  button.id = `button${i}`;
+
+  buttonGroup.append(button);
+}
+
+buttonGroup.addEventListener('click', handleClick);
+
+function handleClick() {
+  const id = event.target.getAttribute('id');
+  [...buttonGroup.children].forEach((button) => {
+    button.id === id 
+    ? document.querySelector(`#${button.id}`).textContent = 'Нажата!'
+    : document.querySelector(`#${button.id}`).textContent = 'Нажми меня!';
+  });
+  count.textContent = counter += 1;
+};
