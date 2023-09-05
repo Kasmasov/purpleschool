148e3944292acc59d090441c8b32afac9de4cafd
kasmasov <kosmos4v@gmail.com>
2023-09-05 13:31:27 +0300

    create const and rewrite function for error catch

--- promise_homework.js
+++ promise_homework.js
@@ -1,18 +1,42 @@
-function getRequest(url, pokemonName, errorMessage) {
+const BASE_URL = 'https://pokeapi.co/api/v2/pokemon/'
+const errorText = 'Проблеммы с запросом, Status = '
+
+
+function getRequest(url, pokemonName) {
   return fetch(`${url}${pokemonName}`)
     .then((response) => {
-      if (!response.ok) {
-        throw new Error(`${errorMessage} ${response.status}`)
-      }
+      getError(response.status);
       return response.json();
     })
 };
 
-const errorText = 'Проблеммы с запросом, Status = '
+function getError(status) {
+  if (status !== 200) {
+    switch (status) {
+      case 400:
+        throw new Error('Bad request! Incorrect syntax of request.');
+      case 401: 
+        throw new Error('Unauthorized! You have to authorize.');
+      case 402:
+        throw new Error('Payment Required! You have to make a payment for services.');
+      case 403:
+        throw new Error('Forbidden! Not enough rights for access.');
+      case 404: 
+        throw new Error('Not Found! The server cannot find the requested resource.')
+      case 405: 
+        throw new Error('Method Not Allowed! The method has been deactivated and can\'t be used.')
+      case 405: 
+        throw new Error('Not Acceptable! Setver  doesn\'t find content!')
+      default:
+        throw new Error('Sorry! Error! We\'re trying to fix problem soon!');
+    }
+  }
 
-getRequest('https://pokeapi.co/api/v2/pokemon/', 'ditto', errorText)
+}
+
+
+getRequest(BASE_URL, 'ditto')
   .then(({ abilities }) => {
-		console.log(abilities)
 		return getRequest(abilities[0].ability.url, '', errorText)
 	})
 	.then(response => {
